76641a92b366fa99cea5fb1ad84eb80a85aea5dd
Carter Bastian <carter.bastian1@gmail.com>
2023-11-30 21:19:28 -0800

    Day 1

--- problems/day_1.py
+++ problems/day_1.py
@@ -1,40 +1,37 @@
 from lib.helpers import log, get_all_strings_by_lines
-
+import re
 
 def part_1():
     input_arr = get_all_strings_by_lines('1.txt')
     log(input_arr)
 
-    cur_max = 0
-    cur_sum = 0
+    cur_prod = 0
     for val in input_arr:
-        # End of group
-        if val == "":
-            if cur_sum > cur_max:
-                cur_max = cur_sum
-            # Update elf index and reset sum
-            cur_sum = 0
-        else:
-            cur_sum += int(val)
+        digits = re.findall(r'\d', val)
+        d = (int(digits[0]) * 10) + (int(digits[-1]))
+        cur_prod += d
 
-    return cur_max
+    return cur_prod
 
 
 def part_2():
     input_arr = get_all_strings_by_lines('1.txt')
+    log(input_arr)
 
-    totals = []
-
-    cur_sum = 0
+    cur_prod = 0
     for val in input_arr:
-        # End of group
-        if val == "":
-            totals += [cur_sum]
-            cur_sum = 0
-        else:
-            cur_sum += int(val)
-    # Don't forget the last one.
-    totals += [cur_sum]
-    log(totals)
+        numbered = re.sub(r'one', 'one1one', val, flags=re.S)
+        numbered = re.sub(r'two', 'two2two', numbered, flags=re.S)
+        numbered = re.sub(r'three', 'three3three', numbered, flags=re.S)
+        numbered = re.sub(r'four', 'four4four', numbered, flags=re.S)
+        numbered = re.sub(r'five', 'five5five', numbered, flags=re.S)
+        numbered = re.sub(r'six', 'six6six', numbered, flags=re.S)
+        numbered = re.sub(r'seven', 'seven7seven', numbered, flags=re.S)
+        numbered = re.sub(r'eight', 'eight8eight', numbered, flags=re.S)
+        numbered = re.sub(r'nine', 'nine9nine', numbered, flags=re.S)
+        digits = re.findall(r'\d', numbered)
+        log(digits)
+        d = (int(digits[0]) * 10) + (int(digits[-1]))
+        cur_prod += d
 
-    return sum(sorted(totals)[-3:])
+    return cur_prod
