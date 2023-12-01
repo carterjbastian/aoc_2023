fbfdb5014e58e3bf73ddb3f050f211f62fff34ec
Carter Bastian <carter.bastian1@gmail.com>
2023-11-30 21:24:19 -0800

    Add comments

--- problems/day_1.py
+++ problems/day_1.py
@@ -7,7 +7,9 @@ def part_1():
 
     cur_prod = 0
     for val in input_arr:
+        # Get the digits
         digits = re.findall(r'\d', val)
+        # Conver with the ~decimal~ system wow!
         d = (int(digits[0]) * 10) + (int(digits[-1]))
         cur_prod += d
 
@@ -20,6 +22,8 @@ def part_2():
 
     cur_prod = 0
     for val in input_arr:
+        # Just replace each word with the word, the digit, and then the word again.
+        # This will keep any words that share letters from getting broken up.
         numbered = re.sub(r'one', 'one1one', val, flags=re.S)
         numbered = re.sub(r'two', 'two2two', numbered, flags=re.S)
         numbered = re.sub(r'three', 'three3three', numbered, flags=re.S)
@@ -29,6 +33,8 @@ def part_2():
         numbered = re.sub(r'seven', 'seven7seven', numbered, flags=re.S)
         numbered = re.sub(r'eight', 'eight8eight', numbered, flags=re.S)
         numbered = re.sub(r'nine', 'nine9nine', numbered, flags=re.S)
+
+        # Use same regex as before to get a list of just the digits
         digits = re.findall(r'\d', numbered)
         log(digits)
         d = (int(digits[0]) * 10) + (int(digits[-1]))
