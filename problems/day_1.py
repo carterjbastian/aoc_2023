2f024a449452742bda4e1b40ea8e797c0e998d74
Carter Bastian <carter.bastian1@gmail.com>
2023-12-01 21:12:54 -0800

    Day 2

--- problems/day_1.py
+++ problems/day_1.py
@@ -24,15 +24,15 @@ def part_2():
     for val in input_arr:
         # Just replace each word with the word, the digit, and then the word again.
         # This will keep any words that share letters from getting broken up.
-        numbered = re.sub(r'one', 'one1one', val, flags=re.S)
-        numbered = re.sub(r'two', 'two2two', numbered, flags=re.S)
-        numbered = re.sub(r'three', 'three3three', numbered, flags=re.S)
-        numbered = re.sub(r'four', 'four4four', numbered, flags=re.S)
-        numbered = re.sub(r'five', 'five5five', numbered, flags=re.S)
-        numbered = re.sub(r'six', 'six6six', numbered, flags=re.S)
-        numbered = re.sub(r'seven', 'seven7seven', numbered, flags=re.S)
-        numbered = re.sub(r'eight', 'eight8eight', numbered, flags=re.S)
-        numbered = re.sub(r'nine', 'nine9nine', numbered, flags=re.S)
+        numbered = re.sub(r'one', 'on1e', val, flags=re.S)
+        numbered = re.sub(r'two', 't2wo', numbered, flags=re.S)
+        numbered = re.sub(r'three', 'thr3ee', numbered, flags=re.S)
+        numbered = re.sub(r'four', 'fo4ur', numbered, flags=re.S)
+        numbered = re.sub(r'five', 'fi5ve', numbered, flags=re.S)
+        numbered = re.sub(r'six', 'si6x', numbered, flags=re.S)
+        numbered = re.sub(r'seven', 'se7ven', numbered, flags=re.S)
+        numbered = re.sub(r'eight', 'ei8ght', numbered, flags=re.S)
+        numbered = re.sub(r'nine', 'ni9ne', numbered, flags=re.S)
 
         # Use same regex as before to get a list of just the digits
         digits = re.findall(r'\d', numbered)
